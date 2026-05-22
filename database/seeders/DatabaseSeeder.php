94499c20509440eeb49ea26023651a3592097e89
SangamPradhan <pradhansangam160@gmail.com>
2026-05-22 20:40:04 +0545

    updated database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Seed additional user details (admin/staff) and roles
+        $this->call(UserDetailsSeeder::class);
     }
 }
