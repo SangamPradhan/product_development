0c09b6971b376e1c2b82596cba645f1d84ca1c25
SangamPradhan <pradhansangam160@gmail.com>
2026-05-30 09:58:26 +0545

    added api route for reviews

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\ReviewController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,6 +11,9 @@ Route::get('/user', function (Request $request) {
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{project}', [ProjectController::class, 'show']);
+Route::get('/projects/{project}/reviews', [ReviewController::class, 'index']);
+Route::post('/projects/{project}/reviews', [ReviewController::class, 'store']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/projects', [ProjectController::class, 'store']);
